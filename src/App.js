aeb0ceae6334b50379f75ea8707e2a2a7a4533fd
전한빛 <asjask123@gmail.com>
2023-04-06 00:13:49 +0900

    🎉 Begin a Project !

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import MainPage from './pages/MainPage'
+import SignUp from './pages/SignUp'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [isLogin, setIsLogin] = useState(false)
+    return (
+        <article className="global-wrapper">
+            <Routes>
+                <Route path="/" element={isLogin || <MainPage />}></Route>
+                <Route path="/signup" element={<SignUp />}></Route>
+                <Route path="/signin" element={<div>로그인창</div>}></Route>
+                <Route path="/todo" element={<div>로그인창</div>}></Route>
+            </Routes>
+        </article>
+    )
 }
 
-export default App;
+export default App
