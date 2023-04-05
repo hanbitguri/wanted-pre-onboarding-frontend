9cc75ff87f7197ab4ca269736bedfe0e7d35f963
전한빛 <asjask123@gmail.com>
2023-04-06 00:18:09 +0900

    🎨 Implement Page : Main

---
+++ src/pages/MainPage.js
@@ -0,0 +1,20 @@
+import '../styles/main-page.css'
+import { Link } from 'react-router-dom'
+import Button from '../components/Button'
+
+function MainPage() {
+    return (
+        <>
+            <div className="main-auth">
+                <Link to={'/signin'}>
+                    <Button className="main-signin" content="로그인" />
+                </Link>
+                <Link to={'/signup'}>
+                    <Button className="main-signup" content="회원가입" />
+                </Link>
+            </div>
+        </>
+    )
+}
+
+export default MainPage
