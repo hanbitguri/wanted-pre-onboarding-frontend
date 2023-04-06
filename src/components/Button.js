760ba1c3682ad15b7e961b549cbe53eab9d3779c
전한빛 <asjask123@gmail.com>
2023-04-06 12:55:20 +0900

    🔨 add to props button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,7 @@ import '../styles/components/button.css'
 
 function Button(props) {
     return (
-        <button className={props.className} data-test-id={props.dti}>
+        <button className={props.className} data-test-id={props.dti} disabled={props.disabled}>
             {props.content}
         </button>
     )
