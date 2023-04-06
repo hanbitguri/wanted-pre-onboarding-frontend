687292217829e93926dbb9ce28d351857fa7af8e
전한빛 <asjask123@gmail.com>
2023-04-06 12:55:58 +0900

    🔨 add to props Input component

--- src/components/Input.js
+++ src/components/Input.js
@@ -2,5 +2,13 @@ import React from 'react'
 import '../styles/components/input.css'
 
 export default function Input(props) {
-    return <input className={props.className} type={props.type} data-test-id={props.dti} />
+    return (
+        <input
+            className={props.className}
+            type={props.type}
+            data-test-id={props.dti}
+            onChange={props.onChangeHandler}
+            onBlur={props.onBlurHandler}
+        />
+    )
 }
