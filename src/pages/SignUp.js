3302d5bed545f6d431b45f6c59250e1dd0ee0b63
전한빛 <asjask123@gmail.com>
2023-04-06 23:55:07 +0900

    🚧 Implement signup logic

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -2,14 +2,17 @@ import '../styles/pages/signup-page.css'
 import Button from '../components/Button'
 import Input from '../components/Input'
 import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function SignUp() {
+    const navigate = useNavigate()
+
     const [isValid, setIsValid] = useState(false)
 
     const [enteredId, setEnteredId] = useState('')
     const [enteredPw, setEnteredPw] = useState('')
 
-    const idRegex = /@{1}/g
+    const idRegex = /.*@.*/gi
     const pwRegex = /.{8,}/gi
 
     const enteredIdisValid = (string) => idRegex.test(string)
@@ -24,16 +27,27 @@ function SignUp() {
         }
         setIsValid(false)
     }
-    const registHandler = () => {
+    const onSubmitHandler = (e) => {
+        e.preventDefault()
         fetch('https://www.pre-onboarding-selection-task.shop/auth/signup', {
             method: 'POST',
-            body: {},
-            headers: 'Content-Type : application/json',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                email: enteredId,
+                password: enteredPw,
+            }),
+        }).then((resp) => {
+            if (resp.ok) {
+                alert('회원가입이 정상적으로 완료되었습니다 !')
+                navigate('/signin')
+            }
         })
     }
 
     return (
-        <form className="signup-form" onSubmit={(e) => e.preventDefault()}>
+        <form className="signup-form" onSubmit={onSubmitHandler}>
             <h2 className="signup-title">회원가입</h2>
             <div className="signup-main">
                 <Input
@@ -41,6 +55,7 @@ function SignUp() {
                     type="text"
                     data-testid="email-input"
                     onChangeHandler={onChangeHandler(setEnteredId)}
+                    placeholder="이메일을 입력해주세요."
                 />
                 <Input
                     className="signup-input-pw"
@@ -48,6 +63,7 @@ function SignUp() {
                     data-testid="password-input"
                     onChangeHandler={onChangeHandler(setEnteredPw)}
                     onBlurHandler={vaildHandler}
+                    placeholder="비밀번호를 입력해주세요."
                 />
                 <Button
                     className="signup-button"
@@ -55,7 +71,6 @@ function SignUp() {
                     content="가입하기"
                     dti="signup-button"
                     disabled={!isValid}
-                    regist={registHandler}
                 ></Button>
             </div>
         </form>
