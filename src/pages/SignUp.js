1b3c2259311b43e77bdc399e10c1a5b1eef1cc80
전한빛 <asjask123@gmail.com>
2023-04-07 15:10:13 +0900

    🚧 attach useInput to Signup Component

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,50 +1,10 @@
 import '../styles/pages/signup-page.css'
 import Button from '../components/Button'
 import Input from '../components/Input'
-import { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useInput } from '../hooks/useInput'
 
 function SignUp() {
-    const navigate = useNavigate()
-
-    const [isValid, setIsValid] = useState(false)
-
-    const [enteredId, setEnteredId] = useState('')
-    const [enteredPw, setEnteredPw] = useState('')
-
-    const idRegex = /.*@.*/gi
-    const pwRegex = /.{8,}/gi
-
-    const enteredIdisValid = (string) => idRegex.test(string)
-    const enteredPwisValid = (string) => pwRegex.test(string)
-    const onChangeHandler = (setState) => {
-        return (e) => setState(e.target.value)
-    }
-    const vaildHandler = () => {
-        if (enteredIdisValid(enteredId) && enteredPwisValid(enteredPw)) {
-            setIsValid(true)
-            return
-        }
-        setIsValid(false)
-    }
-    const onSubmitHandler = (e) => {
-        e.preventDefault()
-        fetch('https://www.pre-onboarding-selection-task.shop/auth/signup', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                email: enteredId,
-                password: enteredPw,
-            }),
-        }).then((resp) => {
-            if (resp.ok) {
-                alert('회원가입이 정상적으로 완료되었습니다 !')
-                navigate('/signin')
-            }
-        })
-    }
+    const [isValid, emailChangeHandler, passwordChangeHandler, vaildHandler, onSubmitHandler] = useInput('', '')
 
     return (
         <form className="signup-form" onSubmit={onSubmitHandler}>
@@ -53,15 +13,15 @@ function SignUp() {
                 <Input
                     className="signup-input-id"
                     type="text"
-                    data-testid="email-input"
-                    onChangeHandler={onChangeHandler(setEnteredId)}
+                    dti="email-input"
+                    onChangeHandler={emailChangeHandler}
                     placeholder="이메일을 입력해주세요."
                 />
                 <Input
                     className="signup-input-pw"
                     type="password"
-                    data-testid="password-input"
-                    onChangeHandler={onChangeHandler(setEnteredPw)}
+                    dti="password-input"
+                    onChangeHandler={passwordChangeHandler}
                     onBlurHandler={vaildHandler}
                     placeholder="비밀번호를 입력해주세요."
                 />
