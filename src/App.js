3850802986b0d3e3d5199b54fabfdc3c046a97f4
전한빛 <asjask123@gmail.com>
2023-04-08 12:34:40 +0900

    ✨ add useEffect Hook to  isLogin

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import MainPage from './pages/MainPage'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
+import TodoList from './pages/TodoList'
 
 function App() {
     const [isLogin, setIsLogin] = useState(false)
+    console.log(isLogin)
+    useEffect(() => {
+        if (localStorage.getItem('access_token') !== null) {
+            setIsLogin(true)
+        }
+        setIsLogin(false)
+    }, [])
+
     return (
         <article className="global-wrapper">
             <Routes>
-                <Route path="/" element={isLogin || <MainPage />}></Route>
+                <Route path="/" element={<MainPage />}></Route>
                 <Route path="/signup" element={<SignUp />}></Route>
                 <Route path="/signin" element={<SignIn />}></Route>
-                <Route path="/todo" element={<div>투두</div>}></Route>
+                <Route path="/todo" element={isLogin ? <TodoList /> : <SignIn />}></Route>
             </Routes>
         </article>
     )
