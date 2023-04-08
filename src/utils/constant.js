a63664f279b786d05015e0a2a774396717fdfeba
전한빛 <asjask123@gmail.com>
2023-04-08 12:35:02 +0900

    ✨ add URL constant file

---
+++ src/utils/constant.js
@@ -0,0 +1,2 @@
+export const AUTH_URL = (path) => `https://www.pre-onboarding-selection-task.shop/auth/${path}`
+export const TODO_URL = (id) => `https://www.pre-onboarding-selection-task.shop/todos/${id ? id : ''}`
