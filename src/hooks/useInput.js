8e3aa9445e04496e8dc10c452f53fd62c603b0dc
전한빛 <asjask123@gmail.com>
2023-04-09 22:56:06 +0900

    🐛 fix a bug

--- src/hooks/useInput.js
+++ src/hooks/useInput.js
@@ -3,80 +3,82 @@ import { useNavigate } from 'react-router-dom'
 import { TODO_URL } from '../utils/constant'
 
 export const useInput = (initValue, initValue2) => {
-    const navigate = useNavigate()
-    const [isValid, setIsValid] = useState(false)
+  const navigate = useNavigate()
+  const [isValid, setIsValid] = useState(false)
 
-    const [enteredValue, setEnteredValue] = useState(initValue)
-    const [enteredValue2, setEnteredValue2] = useState(initValue2)
+  const [enteredValue, setEnteredValue] = useState(initValue)
+  const [enteredValue2, setEnteredValue2] = useState(initValue2)
 
-    const idRegex = /.*@.*/gi
-    const pwRegex = /.{8,}/gi
+  const idRegex = /.*@.*/gi
+  const pwRegex = /.{8,}/gi
 
-    const enteredIdisValid = (string) => idRegex.test(string)
-    const enteredPwisValid = (string) => pwRegex.test(string)
+  const enteredIdisValid = (string) => idRegex.test(string)
+  const enteredPwisValid = (string) => pwRegex.test(string)
 
-    const valueChangeHandler = (e) => {
-        setEnteredValue(e.target.value)
+  const valueChangeHandler = (e) => {
+    setEnteredValue(e.target.value)
+  }
+  const valueChangeHandler2 = (e) => {
+    setEnteredValue2(e.target.value)
+  }
+
+  const vaildHandler = () => {
+    if (enteredIdisValid(enteredValue) && enteredPwisValid(enteredValue2)) {
+      setIsValid(true)
+      return
     }
-    const valueChangeHandler2 = (e) => {
-        setEnteredValue2(e.target.value)
-    }
-
-    const vaildHandler = () => {
-        if (enteredIdisValid(enteredValue) && enteredPwisValid(enteredValue2)) {
-            setIsValid(true)
-            return
+    setIsValid(false)
+  }
+  const onSignUpHandler = (e) => {
+    e.preventDefault()
+    fetch('https://www.pre-onboarding-selection-task.shop/auth/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email: enteredValue,
+        password: enteredValue2,
+      }),
+    }).then((resp) => {
+      if (resp.ok) {
+        alert('회원가입이 정상적으로 완료되었습니다 !')
+        navigate('/signin')
+      }
+    })
+  }
+  const onSigninHandler = (e) => {
+    e.preventDefault()
+    fetch('https://www.pre-onboarding-selection-task.shop/auth/signin', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email: enteredValue,
+        password: enteredValue2,
+      }),
+    })
+      .then((resp) => {
+        if (resp.ok) {
+          return resp.json()
         }
-        setIsValid(false)
-    }
-    const onSignUpHandler = (e) => {
-        e.preventDefault()
-        fetch('https://www.pre-onboarding-selection-task.shop/auth/signup', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                email: enteredValue,
-                password: enteredValue2,
-            }),
-        }).then((resp) => {
-            if (resp.ok) {
-                alert('회원가입이 정상적으로 완료되었습니다 !')
-                navigate('/signin')
-            }
-        })
-    }
-    const onSigninHandler = (e) => {
-        e.preventDefault()
-        fetch('https://www.pre-onboarding-selection-task.shop/auth/signin', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                email: enteredValue,
-                password: enteredValue2,
-            }),
-        })
-            .then((resp) => {
-                if (resp.ok) {
-                    return resp.json()
-                }
-            })
-            .then((key) => {
-                const token = key.access_token
-                localStorage.setItem('access_token', token)
-                navigate('/todo')
-            })
-    }
+      })
+      .then((key) => {
+        const token = key.access_token
+        localStorage.setItem('access_token', token)
+      })
+      .then((key) => {
+        navigate('/todo')
+      })
+  }
 
-    return {
-        isValid,
-        valueChangeHandler,
-        valueChangeHandler2,
-        vaildHandler,
-        onSignUpHandler,
-        onSigninHandler,
-    }
+  return {
+    isValid,
+    valueChangeHandler,
+    valueChangeHandler2,
+    vaildHandler,
+    onSignUpHandler,
+    onSigninHandler,
+  }
 }
